fb2fb910718ef506481679550c2a916c07e9417b
WeiKeLiang <46260753+WeiKeLiang@users.noreply.github.com>
2018-12-30 20:08:42 +0800

    Update Hello_World_.ino

--- Hello_World_.ino
+++ Hello_World_.ino
@@ -5,6 +5,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello Wrold!");
-  delay(1000);
+  Serial.println("Hello,Wrold!");
+  delay(100);
 }
